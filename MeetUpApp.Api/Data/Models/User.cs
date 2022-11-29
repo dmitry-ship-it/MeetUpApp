67294610cbad84ff0f62ad6edccecdd8eda6952f
Dzmitry Tsykman <dimatsikman@gmail.com>
2022-11-29 18:04:50 +0300

    Add UserManager & configs for JWT

--- MeetUpApp.Api/Data/Models/User.cs
+++ MeetUpApp.Api/Data/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MeetUpApp.Api.Data.Models
@@ -6,6 +7,7 @@ namespace MeetUpApp.Api.Data.Models
     /// <summary>
     /// This model is used for auth.
     /// </summary>
+    [Index(nameof(Name), IsUnique = true)]
     public class User
     {
         [Key]
