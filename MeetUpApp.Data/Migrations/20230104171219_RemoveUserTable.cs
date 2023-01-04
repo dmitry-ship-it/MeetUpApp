a95c9a6c8371d707e5dc72843215e1c56d01b699
Dzmitry Tsykman <dimatsikman@gmail.com>
2023-01-04 20:29:44 +0300

    Add missing migrations

---
+++ MeetUpApp.Data/Migrations/20230104171219_RemoveUserTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetUpApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nchar(88)", fixedLength: true, maxLength: 88, nullable: false),
+                    Salt = table.Column<string>(type: "nchar(44)", fixedLength: true, maxLength: 44, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Name",
+                table: "User",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
